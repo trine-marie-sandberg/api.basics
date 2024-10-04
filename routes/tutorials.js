56f11889489d01922aab56f8d8b25d527316d925
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2024-10-04 13:09:08 +0200

    comment

--- routes/tutorials.js
+++ routes/tutorials.js
@@ -6,6 +6,9 @@ var tutorialService = new TutorialService(db);
 const { Op } = require("sequelize");
 
 /* GET tutorials listing. */
+//Filtering example with sequelizer operators (Op)
+//Test querystring:
+//?title=java
 router.get('/', async function(req, res, next) {
   const { title, description, published } = req.query;
   const titleCondition = title ? { title: { [Op.like]: `%${title}%` } } : null;
