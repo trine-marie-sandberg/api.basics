a22c1f039dff35723c6ebd8d452df21cd7d99c97
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2024-10-05 14:08:08 +0200

    adds pagination

--- routes/tutorials.js
+++ routes/tutorials.js
@@ -5,18 +5,31 @@ var db = require("../models");
 var tutorialService = new TutorialService(db);
 const { Op } = require("sequelize");
 
+//Pagination
+const getPagination = (page, size) => {
+  const limit = size ? +size : 5;
+  const offset = page ? (page-1) * limit : 0;
+  return { limit, offset };
+};
+
 /* GET tutorials listing. */
-//Filtering example with sequelizer operators (Op)
-//Test querystring:
+//Filtering, ordering and pagination example with sequelizer operators (Op)
+//Test filter:
 //?title=java
+//Test sort:
+//?sort=key1:asc,key2:desc,key3:asc
+//Test pagination:
+//?page=1&size=4
 router.get('/', async function(req, res, next) {
-  const { title, description, published } = req.query;
+  const { sort, title, description, published, page, size} = req.query;
+  const order = sort ? sort.split(',').map(pair => pair.split(':')) : [];
   const titleCondition = title ? { title: { [Op.like]: `%${title}%` } } : null;
   const descCondition = description ? { description: { [Op.like]: `%${description}%` } } : null;
   const publishedCondition = published ? { published: { [Op.like]: `%${published}%` } } : null;
   const condition = {[Op.and]: [titleCondition, descCondition, publishedCondition]};
-  const tutorials = await tutorialService.getAll(condition);
-  res.render('tutorial', {tutorials: tutorials});
+  const pagination = getPagination(page, size);
+  const tutorials = await tutorialService.getAll(condition, order, pagination);
+  res.render('tutorials', {tutorials: tutorials});
 });
 
 module.exports = router;
--- services/tutorialservice.js
+++ services/tutorialservice.js
@@ -4,9 +4,12 @@ class TutorialService {
         this.tutorial = db.tutorial;
     }
 
-    async getAll(condition) {
+    async getAll(condition, order, pagination) {
         return this.tutorial.findAll({
-            where: condition
+            limit: pagination.limit,
+            offset: pagination.offset,
+            where: condition,
+            order: order
         }).catch(function (err) {
             console.log(err);
         });
